8639b41f324130866e41a46f2cfbf79aa46cb2b6
Diya Pan <diyabunny13@gmail.com>
2026-08-30 22:47:31 -0400

    almost done

--- control.gd
+++ control.gd
@@ -1,17 +1,16 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-	
-
+	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
 	if Input.is_action_just_pressed("interact"):
 		$RichTextLabel2.show()
 		$RichTextLabel3.show()
 		$RichTextLabel4.show()
+		await get_tree().create_timer(5.0).timeout
+		$RichTextLabel2.hide()
+		$RichTextLabel3.hide()
+		$RichTextLabel4.hide()
